687cf5861d34cde85220f67fc05f657cc96724d2
TinaKYF <tinakang9601@gmail.com>
2024-10-15 15:27:45 +1100

    Delete Enhancing-Fairness-and-Utility-in-Dynamic-Matching-Markets-master directory

--- Enhancing-Fairness-and-Utility-in-Dynamic-Matching-Markets-master/Filter.py
+++
@@ -1,7 +0,0 @@
-#去掉含0的经纬度坐标
-import pandas as pd
-taxicsv = pd.read_csv('D:\\MTOP\\On-Demand-Ridesourcing-Project-master\\Data\\2016_03.csv', header=0, dtype='object')
-taxicsv=taxicsv[taxicsv["pickup_longitude"]!="0.0"]
-taxicsv=taxicsv.drop_duplicates(subset=['pickup_longitude','pickup_latitude','dropoff_longitude','dropoff_latitude'])
-
-taxicsv.to_csv("2016_03_filter.csv",index=False)
